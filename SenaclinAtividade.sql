c0b3351b4f463ded2cf6d9364984bc407db7e7df
gustavo <gustavo@gpsbr.net>
2024-05-22 17:38:04 -0300

    feito a table consulta e dentista

--- SenaclinAtividade.sql
+++ SenaclinAtividade.sql
@@ -25,3 +25,38 @@ CONSTRAINT PRIMARY KEY (codPaciente),
 CONSTRAINT CHECK (tipoLogradouro='Praça' OR tipoLogradouro='Rua' OR tipoLogradouro='Avenida' OR tipoLogradouro='Rodovia' OR tipoLogradouro='Viela')
 
 );
+
+CREATE TABLE Dentista (
+codDentista INT AUTO_INCREMENT NOT NULL,
+
+nome VARCHAR(50) NOT NULL,
+
+cro CHAR(8) NOT NULL,
+
+especialidade CHAR(22) NOT NULL,
+
+telefone CHAR (11) NOT NULL,
+
+celular CHAR (14) NOT NULL,
+
+CONSTRAINT PRIMARY KEY (codDentista)
+
+);
+
+ALTER TABLE Dentista
+ADD constraint ck_especialidade check (especialidade= 'Ortodontia' or especialidade= 'geral' or especialidade= 'Periodontia' or especialidade= 'implantodontia' or especialidade= ' ');
+
+CREATE TABLE Consulta (
+codConsulta INT AUTO_INCREMENT NOT NULL, 
+datahora DATETIME NOT NULL,
+tipoConsulta VARCHAR(15) NOT NULL,
+codPaciente INT NOT NULL,
+codDentista INT NOT NULL,
+observacao VARCHAR(100),
+CONSTRAINT PRIMARY KEY (codConsulta),
+CONSTRAINT FK_paciente_consulta FOREIGN KEY (codPaciente) REFERENCES Paciente (codPaciente),
+CONSTRAINT FK_dentista_consulta FOREIGN KEY (codDentista) REFERENCES Dentista (codDentista)
+);
+
+ALTER TABLE Consulta
+ADD constraint ck_tipoConsulta check (tipoconsulta= 'avaliação' or tipoconsulta= 'Tratamento');
